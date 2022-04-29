ef3dab2745af737343ec20bd81883d73007776b2
Robyn Choi <robyn3choi@gmail.com>
2022-04-28 19:07:41 -0700

    initial commit. hardhat and next.js boilerplate

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomiclabs/hardhat-waffle");
+
+require("dotenv").config();
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.13",
+  // defaultNetwork: "polygon_mumbai",
+  networks: {
+    hardhat: {},
+    polygon_mumbai: {
+      url: process.env.ALCHEMY_URL,
+      accounts: [`0x${process.env.WALLET_PRIVATE_KEY}`],
+    },
+  },
+};
